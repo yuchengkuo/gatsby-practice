5c8a05d19cc13c20fda5eec7bd1a9fde18713cc7
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-05-15 16:14:17 +0800

    responsive

--- src/projects/building-first-fs-racecar/process.js
+++ src/projects/building-first-fs-racecar/process.js
@@ -1,7 +1,11 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
-import { Card, Flex } from "@theme-ui/components";
+import { Card, Flex, Grid, Link } from "@theme-ui/components";
 import React from "react";
+import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollToPlugin);
 
 const data = [
   {
@@ -29,11 +33,11 @@ const data = [
 const Process = () => {
   return (
     <>
-      <Flex
+      <Grid
         sx={{
           position: "relative",
-          flexDirection: "row",
-          justifyContent: "space-between",
+          gridTemplateColumns: "repeat(auto-fit, minmax(240px, 1fr))",
+          gap: 10,
           width: "90%",
           mb: 11,
           mx: "auto",
@@ -44,7 +48,22 @@ const Process = () => {
         }}
       >
         {data.map((item, index) => (
-          <Card variant="cards.card" key={index}>
+          <Card
+            onClick={() => {
+              gsap.to(window, {
+                scrollTo: { y: `#${item.title.toLowerCase()}`, offsetY: 50 },
+                ease: "power2.inOut",
+              });
+            }}
+            variant="cards.card"
+            key={index}
+            sx={{
+              textDecoration: "none",
+              transition: "all 200ms ease-out",
+              cursor: "pointer",
+              ":hover": { backgroundColor: "darkGray" },
+            }}
+          >
             <h4 sx={{ variant: "text.heading", fontSize: 2 }}>{item.title}</h4>
             {item.items.map((li, index) => (
               <p
@@ -56,7 +75,7 @@ const Process = () => {
             ))}
           </Card>
         ))}
-      </Flex>
+      </Grid>
     </>
   );
 };
