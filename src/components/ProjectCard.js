419356fc22b76736b9e50aed5ea861c8a636f5d7
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-05-29 21:43:40 +0800

    update scale

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -25,7 +25,7 @@ const ProjectCard = ({ cover, title, tag, slug, badge, color }) => {
         textDecoration: "none",
       }}
       whileHover={{ scale: 1.02 }}
-      whileTap={{ scale: 0.94 }}
+      whileTap={{ scale: 0.97 }}
     >
       {badge?.show && (
         <Badge
