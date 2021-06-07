a5069cb12ab45ce2eff658f3a8d691cc240082da
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-06-07 14:22:39 +0800

    update animation

--- src/components/project/Menu.js
+++ src/components/project/Menu.js
@@ -3,7 +3,7 @@ import { Flex, jsx, Text } from "theme-ui";
 import { transparentize } from "@theme-ui/color";
 import gsap from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 gsap.registerPlugin(ScrollToPlugin);
 
@@ -18,52 +18,57 @@ const Menu = ({ tableOfContents, inView }) => {
     show: { opacity: 1, x: 0 },
   };
   return (
-    <MotionFlex
-      sx={{
-        position: "fixed",
-        backgroundColor: transparentize("darkGray", 0.8),
-        top: 280,
-        left: 8,
-        width: 260,
-        p: 5,
-        flexDirection: "column",
-        gap: 1,
-        borderRadius: 2,
-        boxShadow: "card",
-        display: [`none`, `none`, `flex`],
-      }}
-      variants={box}
-      initial="initial"
-      animate={inView ? "show" : "initial"}
-    >
-      {heading?.map((item, index) => (
-        <MotionText
-          whileTap={() => {
-            gsap.to(window, {
-              scrollTo: { y: `${item.url}`, offsetY: 40 },
-              ease: "power2.inOut",
-            });
-          }}
-          as="p"
-          variant="text.caption"
-          key={index}
+    <AnimatePresence>
+      {inView && (
+        <MotionFlex
           sx={{
-            p: 2,
-            fontSize: 0,
-            color: "lightGray",
-            cursor: "pointer",
-            transition: "all 200ms ease-out",
-            ":hover": {
-              backgroundColor: "primary",
-              color: "background",
-              variant: "shadows.card",
-            },
+            position: "fixed",
+            backgroundColor: transparentize("darkGray", 0.8),
+            top: 280,
+            left: 8,
+            width: 200,
+            p: 5,
+            flexDirection: "column",
+            gap: 1,
+            borderRadius: 2,
+            boxShadow: "card",
+            display: [`none`, `none`, `flex`],
           }}
+          variants={box}
+          initial="initial"
+          animate="show"
+          exit="initial"
         >
-          {item.title}
-        </MotionText>
-      ))}
-    </MotionFlex>
+          {heading?.map((item, index) => (
+            <MotionText
+              whileTap={() => {
+                gsap.to(window, {
+                  scrollTo: { y: `${item.url}`, offsetY: 40 },
+                  ease: "power2.inOut",
+                });
+              }}
+              as="p"
+              variant="text.caption"
+              key={index}
+              sx={{
+                p: 2,
+                fontSize: 0,
+                color: "lightGray",
+                cursor: "pointer",
+                transition: "all 200ms ease-out",
+                ":hover": {
+                  backgroundColor: "primary",
+                  color: "background",
+                  variant: "shadows.card",
+                },
+              }}
+            >
+              {item.title}
+            </MotionText>
+          ))}
+        </MotionFlex>
+      )}
+    </AnimatePresence>
   );
 };
 
